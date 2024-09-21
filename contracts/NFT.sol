c98d09de2cf6007052e93c7602519b3c1a1a7aba
testrobi shahin <testrobishahin@gmail.com>
2024-09-21 09:36:44 +0000

    feat: initiated the project

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 contract TestNFT is ERC721, ERC721Burnable {
     constructor()
-        ERC721("yyy","bbb")
+        ERC721("uuu","iii")
     {}
 
     function safeMint(address to, uint256 tokenId) public {
